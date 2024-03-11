7ebd2dae38c5f5143d09b7347b61c626d8bb5e7a
shivam <dishvam.1652@gmail.com>
2024-03-11 13:49:41 +0530

    spring boot generator

---
+++ src/main/java/com/reports_service/springboot_reports_generator/SpringbootReportsGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.reports_service.springboot_reports_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReportsGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReportsGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reports_service/springboot_reports_generator/SpringbootReportsGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reports_service.springboot_reports_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReportsGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
